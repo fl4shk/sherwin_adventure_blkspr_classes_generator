056328cb26e3108ddbdd16b728d951a514fd40ab
fl4shk <fl4shk@users.noreply.github.com>
2017-08-12 12:52:49 -0400

    Change name of project

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,21 @@
-// This file is part of Sherwin's Adventure Block Classes Generator.
+// This file is part of Sherwin's Adventure Block and Sprite Classes
+// Generator.
 // 
 // Copyright 2017 by Andrew Clark (FL4SHK).
 // 
-// Sherwin's Adventure Block Classes Generator is free software: you can
-// redistribute it and/or modify it under the terms of the GNU General
-// Public License as published by the Free Software Foundation, either
-// version 3 of the License, or (at your option) any later version.
+// Sherwin's Adventure Block and Sprite Classes Generator is free software:
+// you can redistribute it and/or modify it under the terms of the GNU
+// General Public License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
 // 
-// Sherwin's Adventure Block Classes Generator is distributed in the hope
-// that it will be useful, but WITHOUT ANY WARRANTY; without even the
-// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-// See the GNU General Public License for more details.
+// Sherwin's Adventure Block and Sprite Classes Generator is distributed in
+// the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+// the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+// PURPOSE.  See the GNU General Public License for more details.
 // 
 // You should have received a copy of the GNU General Public License along
-// with Sherwin's Adventure Block Classes Generator.  If not, see
-// <http://www.gnu.org/licenses/>.
+// with Sherwin's Adventure Block and Sprite Classes Generator.  If not,
+// see <http://www.gnu.org/licenses/>.
 
 
 
