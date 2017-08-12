6b0972d9840c88b1641ca44f1d0996b0b2926b24
fl4shk <fl4shk@users.noreply.github.com>
2017-08-12 11:55:00 -0400

    First commit

---
+++ src/misc_types.hpp
@@ -0,0 +1,33 @@
+// This file is part of Sherwin's Adventure Block Classes Generator.
+// 
+// Copyright 2015-2017 by Andrew Clark (FL4SHK).
+// 
+// Sherwin's Adventure Block Classes Generator is free software: you can
+// redistribute it and/or modify it under the terms of the GNU General
+// Public License as published by the Free Software Foundation, either
+// version 3 of the License, or (at your option) any later version.
+// 
+// Sherwin's Adventure Block Classes Generator is distributed in the hope
+// that it will be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License along
+// with Sherwin's Adventure Block Classes Generator.  If not, see
+// <http://www.gnu.org/licenses/>.
+
+
+#ifndef misc_types_hpp
+#define misc_types_hpp
+
+#include <cstdint>
+
+typedef uint8_t u8; typedef int8_t s8;
+typedef uint16_t u16; typedef int16_t s16;
+typedef uint32_t u32; typedef int32_t s32;
+typedef uint64_t u64; typedef int64_t s64;
+typedef unsigned int uint;
+
+
+
+#endif		// misc_types-hpp
